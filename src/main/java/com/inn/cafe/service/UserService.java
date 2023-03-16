777b39f6761958a1add63c955ccf397086c44f77
Kasun M.Weerasekara <98303334+Kasunm12@users.noreply.github.com>
2023-03-17 01:04:23 +0530

    update

--- src/main/java/com/inn/cafe/service/UserService.java
+++ src/main/java/com/inn/cafe/service/UserService.java
@@ -6,5 +6,7 @@ import java.util.Map;
 
 public interface UserService {
 
-    ResponseEntity<String> signUp(Map<String,String> requestMap);
+    ResponseEntity<String> signUp(Map<String, String> requestMap);
+
+    ResponseEntity<String> sigunUp(Map<String, String> requestMap);
 }
--- src/main/java/com/inn/cafe/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/inn/cafe/serviceImpl/UserServiceImpl.java
@@ -1,18 +1,43 @@
 package com.inn.cafe.serviceImpl;
 
+import com.inn.cafe.constents.CafeConstants;
 import com.inn.cafe.service.UserService;
+import com.inn.cafe.utils.CafeUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
+@Slf4j
 @Service
 
 public class UserServiceImpl implements UserService {
-    @Slf4j
+
     @Override
     public ResponseEntity<String> signUp(Map<String, String> requestMap) {
         return null;
     }
+
+    @Override
+    public ResponseEntity<String> sigunUp(Map<String, String> requestMap) {
+        log.info("Inside signup{}",requestMap);
+
+        if(validateSignUpMap(requestMap)){
+
+        }else{
+            return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
+        }
+        return null;
+    }
+
+    private boolean validateSignUpMap (Map<String, String> requestMap){
+        if(requestMap.containsKey("name") && requestMap.containsKey("contactNumber")
+                && requestMap.containsKey("email") && requestMap.containsKey("password")){
+            return true;
+        }
+        return false;
+    }
+
 }
